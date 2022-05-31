efac70c1c1364b63b2a4df867124ee6de74c5aa5
PigeonGrass841 <chenghanh@nycstudents.net>
2022-05-30 23:59:32 -0400

    Commit Message

--- src/OasisGUI.java
+++ src/OasisGUI.java
@@ -1,81 +1,68 @@
+/*
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 
-public class OasisGUI {
-    public static void main(String[] args) {
-        OasisGUI graphicUserInterface = new OasisGUI();
-        // Creates the frame
-        JFrame frame = graphicUserInterface.createFrame();
+public class OasisGUI
+{
+    private JTextArea text;
+    private JTextField inputSpace;
+    private LocalVisitor user;
 
-        // Creates the panel at bottom and adds the components
-        JMenuBar menuBar = graphicUserInterface.createMenuBar();
+    // Constructor, which calls helper methods to setup the GUI then load the options
+    public OasisGUI()
+    {
+        text = new JTextArea(20, 35);
+        inputSpace = new JTextField();
+        user = new LocalVisitor();
 
-        // Creates the panel
-        JPanel panel = graphicUserInterface.createPanel();
-
-        // Adds frame components
-        graphicUserInterface.addFrameComponents(frame, menuBar, panel);
+        // Sets up GUI
+        createGUI();
     }
 
-    private JFrame frame;
-    private JMenuBar menuBar;
-    private JPanel panel;
-    private JTextArea textArea;
-    private JTextField input;
+    // Private helper method, called by constructor to set up the GUI and display it
+    private void setupGui()
+    {
+        // Reads from the file line buy line and creates a new Player object
+        LocalVisitor user = LocalVisitor.read();
 
-    // Creates and returns a frame
-    public JFrame createFrame() {
         // Creates a frame
-        frame = new JFrame("Crescent_Glow_Oasis");
+        JFrame frame = new JFrame("Crescent_Glow_Oasis");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1200, 675);
 
-        // Returns the frame
-        return frame;
-    }
+        // Middle panel with text
+        JPanel movieListPanel = new JPanel();
+        // Welcomes the user
+        text.setText(user.welcome());
+        text.setFont(new Font("Helvetica", Font.PLAIN, 16));
+        text.setWrapStyleWord(true);
+        text.setLineWrap(true);
+        movieListPanel.add(text);
 
-    // Creates the menu bar and adds components
-    public JMenuBar createMenuBar() {
-        // Creates a menu bar
-        menuBar = new JMenuBar();
-        JMenu save = new JMenu("Save");
-        menuBar.add(save);
-
-        // Adds the components
-        JMenuItem sContinue = new JMenuItem("Save and continue");
-        JMenuItem sQuit = new JMenuItem("Save and quit");
-        save.add(sContinue);
-        save.add(sQuit);
-
-        // Returns the menu bar
-        return menuBar;
-    }
-
-    // Creates the panel
-    public JPanel createPanel() {
-        // The panel is not visible in output
-        panel = new JPanel();
+        // Bottom panel with text field and buttons
+        JPanel panel = new JPanel(); // The panel is not visible in output
         JLabel label = new JLabel("Choice: ");
+
         // Accepts up to 18 characters
-        input = new JTextField(10);
+        inputSpace = new JTextField(10);
         JButton enter = new JButton("Enter");
+
         // Components Added using Flow Layout
         panel.add(label);
-        panel.add(input);
+        panel.add(inputSpace);
         panel.add(enter);
 
-        return panel;
-    }
+        // Prompts the user for a choice
+        System.out.println("\nWelcome to the Crescent Glow Oasis. You can fish and sell or buy items in the shop.");
+        System.out.println("[1] Go fishing\n[2] Enter the shop\n[3] Check inventory and sum\n[4] Resort to capitalism\n[5] Leave");
 
-    public void addFrameComponents(JFrame frame, JMenuBar menuBar, JPanel panel) {
-        // Text Area at the Center
-        textArea = new JTextArea();
+        // PART 2 -- SET UP EVENT HANDLING
+        // Setting up buttons to use ActionListener interface and actionPerformed method
+        enter.addActionListener(this);
 
-        //Adding Components to the frame.
-        frame.getContentPane().add(BorderLayout.NORTH, menuBar);
-        frame.getContentPane().add(BorderLayout.SOUTH, panel);
-        frame.getContentPane().add(BorderLayout.CENTER, textArea);
+        // Showing the frame
+        frame.pack();
         frame.setVisible(true);
     }
 
@@ -89,10 +76,93 @@ public class OasisGUI {
         {
             // Retrieves the user input
             // getText() returns a string and converts it to an integer
-            String choice = input.getText();
+            String choice = inputSpace.getText();
 
             // Executes the corresponding method
+            // Choice [1] Go fishing
+            if (choice.equals("1"))
+            {
+                // Creates the FishPond object
+                FishPond oasis = new FishPond(user);
+                oasis.fish();
+            }
+            // Choice [2] Enter the shop
+            if (choice.equals("2"))
+            {
+                // Creates the ItemShop object
+                ItemShop counter = new ItemShop(user);
 
+                // Prompts the user to sell or buy something
+                System.out.println("\nThanks for coming to the shop, would you like to sell or buy something?");
+                System.out.println("[1] Sell something\n[2] Buy something");
+                System.out.print("Choice: ");
+
+                // Choice [1] Sell something
+                if (choice.equals("1"))
+                {
+                    // Prompts the user to sell an item or all items in their inventory
+                    System.out.println("\n[1] Sell an item\n[2] Sell all items");
+                    System.out.print("Choice: ");
+
+                    // Prompts user input
+                    choice = input.nextLine();
+
+                    // Choice [1] Sell an item
+                    if (choice.equals("1"))
+                    {
+                        counter.sellItem();
+                    }
+                    // Choice [2] Sell all items
+                    if (choice.equals("2"))
+                    {
+                        counter.sellItemAll();
+                        choice = "1";
+                    }
+                }
+                // Choice [2] Buy something
+                if (choice.equals("2"))
+                {
+                    // Prompts the user to buy an item or buy multiple instance of the same item using their entire sum
+                    System.out.println("\n[1] Buy an item\n[2] Buy an item in bulk");
+                    System.out.print("Choice: ");
+
+                    //Prompts user input
+                    choice = input.nextLine();
+
+                    // Choice [1] Buy an item
+                    if (choice.equals("1"))
+                    {
+                        counter.buyItem();
+                    }
+                    // Choice [2] Buy an item in bulk
+                    if (choice.equals("2"))
+                    {
+                        counter.buyItemAll();
+                    }
+                }
+            }
+            // Choice [3] Check inventory and sum
+            if (choice.equals("3"))
+            {
+                System.out.println(user);
+            }
+            // Choice [4] Resort to capitalism
+            if (choice.equals("4"))
+            {
+                McDonalads job = new McDonalads();
+                job.work(user);
+            }
+            // Choice [5] Quit
+            if (choice.equals("5"))
+            {
+
+            }
+
+            // Prompts the user for a choice
+            System.out.println("\nYou can fish and sell or buy items in the shop.");
+            System.out.println("[1] Go fishing\n[2] Enter the shop\n[3] Check inventory and sum\n[4] Resort to capitalism\n[5] Leave");
+            System.out.print("Choice: ");
         }
     }
 }
+*/
